470eacd537ab360a742122347fbc42625114fe94
tamicarvalho <tamy.cases@hotmail.com>
2023-10-15 15:47:52 -0300

    Calculator Test

--- cypress/e2e/calculator.cy.ts
+++ cypress/e2e/calculator.cy.ts
@@ -17,27 +17,119 @@ describe ("Operation", () => {
      */
     
     it("should add two numbers", () => {
-        cy.get(':nth-child(14)').click()
+        cy.get("[data-test='2']").should("be.visible").click() // The user click on the number "2" button
+
+        cy.get("[data-test='results']").should("have.value", "2") // Check if the number "2" is displayed on the screen
+
+        cy.get("[data-test='+']").should("be.visible").click() // The user click on the operator "+" button
+
+        cy.get("[data-test='results']").should("have.value", "2+") // Check if the "2+" is displayed on the screen
+
+        cy.get("[data-test='2']").should("be.visible").click() // The user click on the number "2" button
+
+        cy.get("[data-test='results']").should("have.value", "2+2") // Check if the "2+2" is displayed on the screen
+
+        cy.get("[data-test='=']").should("be.visible").click() // The user click on the operator "=" button
+
+        cy.get("[data-test='results']").should("have.value", "4") // Check if the result is "4"
+        
 
 
     })
+       /**
+     * gherkin
+     * Scenario: Should Subtract two numbers
+     *  When the user click on the number "5" button
+     *  Then the number "5" should be displayed on the screen
+     *  When the user click on the operator "-" button
+     *  Then the "5-" should be displayed on the screen
+     *  When the user click on the number "2" button
+     *  Then the "5-2" should be displayed on the screen
+     *  When the user click on the operator "=" button
+     *  Then the result should be "3"
+     */
 
+         it("should subtract two numbers", () => {
+            cy.get("[data-test='5']").should("be.visible").click() // The user click on the number "5" button
 
+            cy.get("[data-test='results']").should("have.value", "5") // Check if the number "5" is displayed on the screen
 
+            cy.get("[data-test='-']").should("be.visible").click() // The user click on the operator "-" button
+
+            cy.get("[data-test='results']").should("have.value", "5-") // Check if the "5-" is displayed on the screen
+
+            cy.get("[data-test='2']").should("be.visible").click() // The user click on the number "2" button
+
+            cy.get("[data-test='results']").should("have.value", "5-2") // Check if the "5-2" is displayed on the screen
+
+            cy.get("[data-test='=']").should("be.visible").click() // The user click on the operator "=" button
+
+            cy.get("[data-test='results']").should("have.value", "3") // Check if the result is "3"
+
+         })
+
+                 /**
+     * gherkin
+     * Scenario: Should Divide two numbers
+     *  When the user click on the number "4" button
+     *  Then the number "4" should be displayed on the screen
+     *  When the user click on the operator "/" button
+     *  Then the "4/" should be displayed on the screen
+     *  When the user click on the number "2" button
+     *  Then the "4/2" should be displayed on the screen
+     *  When the user click on the operator "=" button
+     *  Then the result should be "2"
+     */
+
+             it("should divide two numbers", () => {
+                cy.get("[data-test='4']").should("be.visible").click() // The user click on the number "4" button
     
-})
+                cy.get("[data-test='results']").should("have.value", "4") // Check if the number "4" is displayed on the screen
+    
+                cy.get("[data-test='/']").should("be.visible").click() // The user click on the operator "/" button
+    
+                cy.get("[data-test='results']").should("have.value", "4/") // Check if the "4/" is displayed on the screen
+    
+                cy.get("[data-test='2']").should("be.visible").click() // The user click on the number "2" button
+    
+                cy.get("[data-test='results']").should("have.value", "4/2") // Check if the "4/2" is displayed on the screen
+    
+                cy.get("[data-test='=']").should("be.visible").click() // The user click on the operator "=" button
+    
+                cy.get("[data-test='results']").should("have.value", "2") // Check if the result is "2"
+    
+             })
+    
+                 /**
+     * gherkin
+     * Scenario: Should Multiply two numbers
+     *  When the user click on the number "2" button
+     *  Then the number "4" should be displayed on the screen
+     *  When the user click on the operator "*" button
+     *  Then the "2*" should be displayed on the screen
+     *  When the user click on the number "6" button
+     *  Then the "2*6" should be displayed on the screen
+     *  When the user click on the operator "=" button
+     *  Then the result should be "12"
+     */
 
-
-/** 
-* gherkin
-* Scenario: Should login with a valid user
-*   Given there is the following user in the system:
-*     | email       | password |
-*     | bob@bob.bob | changeme |
-*   When the user navigates to the login page
-*   And type 'bob@bob.bob' on the email field
-*   And type 'changeme' on the password field
-*   And clicks the 'ENTRAR' button
-*   Then the user should be redirected to the links page
-* 
-*/
+                    it("should multiply two numbers", () => {
+                    cy.get("[data-test='2']").should("be.visible").click() // The user click on the number "2" button
+            
+                    cy.get("[data-test='results']").should("have.value", "2") // Check if the number "2" is displayed on the screen
+            
+                    cy.get("[data-test='*']").should("be.visible").click() // The user click on the operator "*" button
+            
+                    cy.get("[data-test='results']").should("have.value", "2*") // Check if the "2*" is displayed on the screen
+            
+                    cy.get("[data-test='6']").should("be.visible").click() // The user click on the number "6" button
+            
+                    cy.get("[data-test='results']").should("have.value", "2*6") // Check if the "2*6" is displayed on the screen
+            
+                    cy.get("[data-test='=']").should("be.visible").click() // The user click on the operator "=" button
+            
+                    cy.get("[data-test='results']").should("have.value", "12") // Check if the result is "12"
+            
+                    })
+            
+                })
